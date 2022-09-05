c03f9e6e8c0a2459f7e48f05c2471aadb66f66d7
Pelin Hangişi <pelinhangisi@gmail.com>
2022-09-06 02:21:48 +0300

    initial commit

---
+++ src/main/java/com/pelinhangisi/springboottodoapp/SpringBootTodoappApplication.java
@@ -0,0 +1,13 @@
+package com.pelinhangisi.springboottodoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTodoappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTodoappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pelinhangisi/springboottodoapp/SpringBootTodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pelinhangisi.springboottodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTodoappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
